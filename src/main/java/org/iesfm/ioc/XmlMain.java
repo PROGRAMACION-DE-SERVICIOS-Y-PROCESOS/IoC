91fcdf23ba62d502e306516e5046d27ff1c4584a
mikelsanvi <mikelsanvi@gmail.com>
2021-10-13 13:24:46 +0200

    first commit

---
+++ src/main/java/org/iesfm/ioc/XmlMain.java
@@ -0,0 +1,14 @@
+package org.iesfm.ioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class XmlMain {
+
+    public static void main(String[] args)  {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("instituto-app.xml");
+        InstitutoPrograma programa = context.getBean(InstitutoPrograma.class);
+        programa.ejecutar();
+    }
+}
